8c2b4003723e37fd9ee6c97c9c29ea0ba02df412
onur <onurkanca3@gmail.com>
2020-02-05 00:26:45 +0300

    "bootstrap3 downloaded, and added some features"

--- udemyProject1/src/app/recipes/recipe-list/recipe-list.component.ts
+++ udemyProject1/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('TestRecipe',"This is just a test !","https://assets.bwbx.io/images/users/iqjWHBFdfxIU/ibFW56Nof_Jk/v0/1000x-1.jpg")
+  ];
   constructor() { }
 
   ngOnInit() {
